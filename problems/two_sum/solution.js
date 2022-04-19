012388dd7c21274b8b655f40bfc3f8b0ddcf3b43
LanceHebert <81220452+LanceHebert@users.noreply.github.com>
2022-04-19 20:11:10 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -3,24 +3,64 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// create an empty hash map, fill with numbers with their index. Take target - current value and find other input, search for that in hash map. 
+
 var twoSum = function(nums, target) {
-    
-    let otherNum = 0 
-    answerArray = []
-    
-    for (let i=0;i<nums.length;i++)
-    {        
-         otherNum = target - nums[i]
-         if (nums.indexOf(otherNum) !== (-1) && nums.indexOf(otherNum) !== nums.indexOf(nums[i])){
-             answerArray.push(i)
-             answerArray.push(nums.indexOf(otherNum))
-             return answerArray
-         }
-        else if (nums.indexOf(otherNum) !== (-1) && (nums.indexOf((otherNum),(i+1))) !== -1 )
+   let hash = {}
+   
+   for(let i = 0;i<nums.length; i++)
+       {
+           hash[nums[i]] = i
+       }
+   
+    for(let i = 0;i<nums.length; i++)
+    {
+        if((target - nums[i]) in hash && hash[target-nums[i]] !== i )
             {
-                answerArray.push(i)
-                answerArray.push(nums.indexOf((otherNum),(i+1)))
-                return answerArray
-            }              
+                return [i,hash[target-nums[i]]]
+            }
     }
-}
+    
+};
+
+
+
+
+
+
+
+// var twoSum = function(nums, target) {
+//   let hash = {}
+//   let diff = 0
+//   let answer =[]
+  
+//   for(let i = 0; i < nums.length ; i++)
+//     {
+//         hash[nums[i]]= i
+//     }
+//     console.log(hash)
+    
+//     for(let i = 0; i < nums.length ; i++)
+//         {
+//           diff = target - nums[i]
+            
+//               if(diff in hash && i !== hash[diff])
+//                 {
+//                 return answer=[i,hash[diff]]                    
+//                 }        
+//         }    
+   
+// };
+
+//  var twoSum = function (nums, target) {
+//   let obj = {}
+//   for (let i = 0; i < nums.length; i++) {
+//     if (target - nums[i] in obj) {
+//       return [obj[target - nums[i]], i]
+//     } else {
+//       obj[nums[i]] = i;
+//     }
+//   }
+// };
+
