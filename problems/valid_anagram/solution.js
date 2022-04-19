cb9d68d0eff73561de0e9d50a50a623ae04eab44
LanceHebert <81220452+LanceHebert@users.noreply.github.com>
2022-04-19 20:56:15 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

--- problems/valid_anagram/solution.js
+++ problems/valid_anagram/solution.js
@@ -3,33 +3,56 @@
  * @param {string} t
  * @return {boolean}
  */
+
+// Two strings , s,t ,  return true if t is anagram of s . false otherwise
+// Create a hashmap of s, run a second for loop that decrements the amount s key has.
+
+
 var isAnagram = function(s, t) {
-//     create an object and find frequency of each letter in str1, then loop through str2 and subtract each letter from str1 based on what str2 is
-    
-    
-     if (s.length !== t.length) {
-        console.log(false);
-      return false;
+    if(s.length !== t.length){
+        return false
     }
-         
-    let sHash = {}
     
-    for(let letter of s)
-        {
-            sHash[letter] = (sHash[letter] || 0) + 1         
-            
-        }
-    
-    for(let letter  of t)
-        {
-            if(!(sHash[letter])) 
-                {
-                    return false
-                }
-            else{
-                sHash[letter]-- 
-            }
-        }
-    return true
+   let hash = {}
    
+   for(let i =0; i<s.length;i++){
+       hash[s[i]] = (hash[s[i]] || 0) + 1
+   }
+   
+   for(let i =0; i< s.length;i++){
+      
+        if(!(hash[t[i]]))
+        {
+          return false
+        }
+       hash[t[i]]--  
+   }
+    
+   return true
 };
+
+
+//   if (s.length !== t.length) {
+//         console.log(false);
+//       return false;
+//     }
+         
+//     let sHash = {}
+    
+//     for(let letter of s)
+//         {
+//             sHash[letter] = (sHash[letter] || 0) + 1         
+            
+//         }
+    
+//     for(let letter  of t)
+//         {
+//             if(!(sHash[letter])) 
+//                 {
+//                     return false
+//                 }
+//             else{
+//                 sHash[letter]-- 
+//             }
+//         }
+//     return true
