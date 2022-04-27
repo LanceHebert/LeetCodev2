aeb307791266c7c24315a6a7707ad69f10d30cd8
LanceHebert <81220452+LanceHebert@users.noreply.github.com>
2022-04-27 01:25:54 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -4,25 +4,68 @@
  * @return {number[]}
  */
 
-// create an empty hash map, fill with numbers with their index. Take target - current value and find other input, search for that in hash map. 
+// array of nums, and target . return the indices that add up to target
+
+// target - num = diff, iterate through the array. At each value take target - nums[i] = diff. see if diff is in hash, if it is return the indexs. If it is not create the hash with the index as teh value.
 
 var twoSum = function(nums, target) {
-   let hash = {}
-   
-   for(let i = 0;i<nums.length; i++)
-       {
-           hash[nums[i]] = i
-       }
-   
-    for(let i = 0;i<nums.length; i++)
-    {
-        if((target - nums[i]) in hash && hash[target-nums[i]] !== i )
-            {
-                return [i,hash[target-nums[i]]]
-            }
-    }
     
+    let hash ={}
+    
+    for(let i = 0; i < nums.length;i++){
+        let diff = target - nums[i]
+        
+        if(diff in hash){
+            return [i,hash[diff]]
+        }
+        hash[nums[i]]=i
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    let hash = {}
+   
+//    for(let i = 0;i<nums.length; i++)
+//        {
+//            hash[nums[i]] = i
+//        }
+   
+//     for(let i = 0;i<nums.length; i++)
+//     {
+//         if((target - nums[i]) in hash && hash[target-nums[i]] !== i )
+//             {
+//                 return [i,hash[target-nums[i]]]
+//             }
+//     }
+
+
+
 
 
 
