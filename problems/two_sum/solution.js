72632e2d861df40b8feb2e33275a7b1cdfd80053
LanceHebert <81220452+LanceHebert@users.noreply.github.com>
2022-04-28 01:05:32 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -4,106 +4,23 @@
  * @return {number[]}
  */
 
-// array of nums, and target . return the indices that add up to target
+// array nums, with target. return indices of two numbers that add up to target.
 
-// target - num = diff, iterate through the array. At each value take target - nums[i] = diff. see if diff is in hash, if it is return the indexs. If it is not create the hash with the index as teh value.
+// Create a hash map with the value being the index.
+
+// I need to take target - iterated value to get difference. Then check if difference exists in the hash. Use that index plus the index that is the value from the hash and return it 
+
 
 var twoSum = function(nums, target) {
+    let hash = {}
     
-    let hash ={}
-    
-    for(let i = 0; i < nums.length;i++){
+    for(let i=0;i < nums.length;i++){
         let diff = target - nums[i]
         
-        if(diff in hash){
-            return [i,hash[diff]]
+        if(hash[diff] != null){
+            console.log(hash[diff])
+            return ([i,hash[diff]])
         }
-        hash[nums[i]]=i
+        hash[nums[i]] = i
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    let hash = {}
-   
-//    for(let i = 0;i<nums.length; i++)
-//        {
-//            hash[nums[i]] = i
-//        }
-   
-//     for(let i = 0;i<nums.length; i++)
-//     {
-//         if((target - nums[i]) in hash && hash[target-nums[i]] !== i )
-//             {
-//                 return [i,hash[target-nums[i]]]
-//             }
-//     }
-
-
-
-
-
-
-
-
-
-
-// var twoSum = function(nums, target) {
-//   let hash = {}
-//   let diff = 0
-//   let answer =[]
-  
-//   for(let i = 0; i < nums.length ; i++)
-//     {
-//         hash[nums[i]]= i
-//     }
-//     console.log(hash)
-    
-//     for(let i = 0; i < nums.length ; i++)
-//         {
-//           diff = target - nums[i]
-            
-//               if(diff in hash && i !== hash[diff])
-//                 {
-//                 return answer=[i,hash[diff]]                    
-//                 }        
-//         }    
-   
-// };
-
-//  var twoSum = function (nums, target) {
-//   let obj = {}
-//   for (let i = 0; i < nums.length; i++) {
-//     if (target - nums[i] in obj) {
-//       return [obj[target - nums[i]], i]
-//     } else {
-//       obj[nums[i]] = i;
-//     }
-//   }
-// };
-
