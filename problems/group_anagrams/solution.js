d686c48ff6c24950381240d365ad21ba44fb8d4e
LanceHebert <81220452+LanceHebert@users.noreply.github.com>
2022-05-01 06:14:19 +0000

    Sync LeetCode submission - Group Anagrams (javascript)

--- problems/group_anagrams/solution.js
+++ problems/group_anagrams/solution.js
@@ -3,31 +3,26 @@
  * @return {string[][]}
  */
 
-// array of strings strs. Group anagrams together. return an array of arrays 
+//  array of strings. group the anagrams together return answer in an array of grouped arrays.
 
-// first we need to split each string so we can break it apart into letter components
+//  I want to iterate through the array and separate out each string. Then take that string and split it and sort it. then create a hash where the key is the sorted letters and the value is the original string
+// then use Object.values(hash) to display the arrays.
 
-//in the value for each key which is the pattern of letters set the string so that each key holds an array of strings that match 
-
-// iterate through the hash returning only the values into a big array to hold the smaller arrays
 
 var groupAnagrams = function(strs) {
     let hash = {}
     
-    
-    for(let i =0;i<strs.length;i++){
-        let splitStrs = strs[i].split("")
-        splitStrs.sort()
+    for (let i = 0; i < strs.length;i++){
+        let splitSortedString = strs[i].split("").sort()
+        console.log(strs[i])
         
-        
-        
-        if(!(splitStrs in hash)){
-           hash[splitStrs] = [strs[i]] 
+        if(hash[splitSortedString]){
+            hash[splitSortedString].push(strs[i]) 
+        }else{
+            hash[splitSortedString] = [strs[i]]
         }
-        else{
-            hash[splitStrs].push(strs[i])
-        }               
+           
+        
     }
-    return Object.values(hash)
-    
+     return Object.values(hash) 
 };
