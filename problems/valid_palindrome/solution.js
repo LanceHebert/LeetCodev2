c1de781827c6693c3a4beac9588dbe1cb2a22894
LanceHebert <81220452+LanceHebert@users.noreply.github.com>
2022-05-04 23:29:17 +0000

    Sync LeetCode submission - Valid Palindrome (javascript)

--- problems/valid_palindrome/solution.js
+++ problems/valid_palindrome/solution.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 
-// given string s, return true if it can be read backwards and forwards
-
-// first we need to strip out all "excess" characters that are non alphanumeric with reg ex.
-
-// then we split the entire string into component parts and we can check if a start pointer === end pointer we increment and decrement them each by one until the pointers meet and exit out and return true 
+// s is a string
+// filter down to just alphanumerics, convert to lowercase
+// left and right pointer and move them together checking to see if they equal
 
 var isPalindrome = function(s) {
-    
-    let reg = /[\W_]/g 
+    let reg = /\W|_/g
     let regS = s.toLowerCase().replace(reg,"")
     
     let left = 0
@@ -20,50 +17,10 @@ var isPalindrome = function(s) {
     while(left < right){
         if(regS[left] !== regS[right]){
             return false
-        }else{
+        }
         left++
         right--
-        }
-        
     }
-    return true
+    return true  
     
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let reg = /[\W_]/g
-//     let str=s.toLowerCase().replace(reg,"")
-//     let left =0    
-//     let arr = str.split("")
-//     let right=arr.length - 1 
-    
-//     while(left < right){
-//         if(arr[left]!==arr[right] )
-//             {
-//                 return false
-//             }
-//         left++
-//         right--        
-//     }
-//     return true
