f8233e70c6cd67a283558e362099a143d16d7e69
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-05-18 19:37:49 -0700

    Time: 130 ms (9.15%), Space: 45.7 MB (9.58%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+
+
+
+
+
+// i=string s
+// o= return if stringis valid w/ pairs
+// edgecase: odd number auto false
+
+var isValid = function(s) {
+        
+    if(s.length % 2 !== 0){
+        return false
+    }
+    
+    const hash = { "(":")","[":"]","{":"}"}
+    const stack =[]
+    
+    for(let char of s){
+        console.log(char)
+        if(hash[char]){
+            stack.push(hash[char])
+        }else{
+            if( stack.pop() !== char ) return false
+        }
+    }
+  return (!stack.length)
+    
+};
+
+// [][][]
