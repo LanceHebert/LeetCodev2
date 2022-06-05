4bedd693a30ac2340e3254eff2e87d1e9b76f545
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-04 22:44:13 -0700

    Time: 151 ms (33.57%), Space: 51.4 MB (33.01%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,18 +3,18 @@
  * @return {boolean}
  */
 
-// i:array nums
-// o:return true if appears twice,false otherwise
+// i:array nums , O:return bool , E:,
+
 
 var containsDuplicate = function(nums) {
     
     const hash = {}
     
-    for(let i = 0; i<nums.length;i++){
+    for(let i = 0 ; i < nums.length; i++){
         hash[nums[i]] = hash[nums[i]] + 1 || 1
         
         if(hash[nums[i]] > 1){
-            return true;
+            return true
         }
     }
     return false
