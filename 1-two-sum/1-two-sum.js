f45a5e6c7bd12afc096f46848f0f44edcab9c061
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-04 23:12:43 -0700

    Time: 78 ms (82.19%), Space: 42.5 MB (68.55%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,22 +4,26 @@
  * @return {number[]}
  */
 
-// i=array nums, integer target
-// o=return indices of two numbers that add up to target
+// I:array nums. int target
+// O:return indices that add up to target
+// E:
 
 var twoSum = function(nums, target) {
+    
     const hash = {}
     
-    for(let i =0;i<nums.length;i++){
+    for(let i = 0 ; i < nums.length;i++){
+        
         
         let diff = target - nums[i]
         
-        if(diff in hash && hash[diff] !== i){
-            return [i,hash[diff]]
-        }  
-        else{
-           hash[nums[i]] = i 
-        }
+       if(diff in hash){
+           return [i,hash[diff]]
+       }else{
+           hash[nums[i]] =i
+       }
+        
     }
+    
     
 };
