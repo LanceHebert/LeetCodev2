677b316a589e86933661178408289fecdafdf6b7
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-06 21:40:57 -0700

    Time: 75 ms (98.77%), Space: 51.7 MB (71.57%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+// i:array prices o:return max profit,return 0 otherwise e:sorted decreaseing, or empty
+
+
+
+var maxProfit = function(prices) {
+    
+   let buyDay = prices[0]
+   let profit = 0
+   
+   for(let i = 0; i < prices.length;i++){
+       if(buyDay > prices[i]){
+          buyDay = prices[i]
+       }
+       
+       if(prices[i] - buyDay > profit){
+           profit = prices[i] - buyDay
+       }
+       
+   }
+    return profit
+    
+    
+};
