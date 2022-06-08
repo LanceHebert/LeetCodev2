593c7c8171660c3196caefc4d3756f354a34adb8
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-07 19:17:29 -0700

    Time: 91 ms (78.21%), Space: 42.8 MB (86.26%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,31 +4,27 @@
  * @return {boolean}
  */
 
-// i:two strings s and t
-// O:return true if anagram
-// e:need to be same length
+
 
 
 var isAnagram = function(s, t) {
-    if(s.length !== t.length) return false
     
-    const sHash = {}
+    if(s.length !== t.length){
+        return false
+    }
+    const sHash ={}
     
     for(let i = 0; i < s.length; i++){
-        sHash[s[i]] = sHash[s[i]] + 1 || 1   
+        sHash[s[i]] = sHash[s[i]] + 1 || 1     
         
     }
-    
-    for(let letter of t){
-        if(sHash[letter]){
-            sHash[letter]--
-        }else{
+    for(let i = 0; i < s.length; i++){
+        if(!sHash[t[i]]){
             return false
         }
+        sHash[t[i]]--
     }
+    return true
 
     
-   
-    
-    return true
 };
