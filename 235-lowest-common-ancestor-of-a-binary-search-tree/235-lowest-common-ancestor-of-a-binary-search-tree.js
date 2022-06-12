fa41614a5fde407ca946a526afd6e72a6db8f37c
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-12 14:33:37 -0700

    Time: 85 ms (91.51%), Space: 52.3 MB (47.51%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,14 +14,18 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     if(root === null) return null
+        
+    if(root.val === p.val || root.val === q.val || root === null){
+        return root
+    }
     
-    if((root.val === p.val) || (root.val === q.val)) return root
-    
-    let left = lowestCommonAncestor(root.left,p,q)
-    let right = lowestCommonAncestor(root.right,p,q)
-    
-    if(left===null) return right
+  let left = lowestCommonAncestor(root.left,p,q)
+  let right = lowestCommonAncestor(root.right,p,q)
+  
+  if(left === null) return right
     if(right === null) return left
+   
     
     return root
+    
 };
