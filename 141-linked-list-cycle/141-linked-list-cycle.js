2058efd094c96f4ceed8e78f2cd63cf73a38d0b3
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-12 16:01:46 -0700

    Time: 96 ms (55.30%), Space: 45.3 MB (26.71%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -11,16 +11,16 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+    let slow = head
+    let fast = head
     
-   let slow = head
-   let fast = head
-   
-   while(fast && fast.next){
-       fast = fast.next.next
-       slow = slow.next
-       
-       if(slow === fast) return true
-   }
+    while(fast && fast.next){
+      fast = fast.next.next
+        slow = slow.next
+        
+        if(slow === fast) return true
+    }
     return false
+    
+    
 };
-
