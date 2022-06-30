fd727a61bda73bf788a6b69aedeb906557b5062a
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-30 14:13:25 -0700

    Time: 115 ms (58.42%), Space: 51.4 MB (91.07%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,26 +3,25 @@
  * @return {number}
  */
 
-// i:array prices o:return max profit,return 0 otherwise e:sorted decreaseing, or empty
-
+// buy day gets checked vs following iterated days,if buy day is greater replace
+// if current day - buy day is greater than max profit increase max profit
 
 
 var maxProfit = function(prices) {
     
-   let buyDay = prices[0]
-   let profit = 0
-   
-   for(let i = 1; i < prices.length;i++){
-       if(buyDay > prices[i]){
-          buyDay = prices[i]
-       }
-       
-       if(prices[i] - buyDay > profit){
-           profit = prices[i] - buyDay
-       }
-       
-   }
-    return profit
+    let buyDay = prices[0]
+    let maxProfit = 0
     
+    for(let i=0;i < prices.length;i++){
+        if(buyDay > prices[i]){
+            buyDay = prices[i]
+        }
+        
+        if(prices[i]-buyDay > maxProfit){
+            maxProfit = prices[i]-buyDay
+        }
+        
+    }
+    return maxProfit
     
 };
