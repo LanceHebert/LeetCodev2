2734e4593a9292234d188e00c0947a41bf7b7a18
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-30 13:23:32 -0700

    Time: 130 ms (12.91%), Space: 44.3 MB (47.48%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -10,32 +10,45 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+
+// iterate the total length of l1+l2, choose one to start, and then move pointer to next, recheck if less or greater than, repeat process until next = null, if one list is null .next is the rest of the other list
+
+
 var mergeTwoLists = function(list1, list2) {
     
-    const mergeList = new ListNode(-Infinity)
-    let node = mergeList
+    const resultList = new ListNode(-Infinity)
+    let node = resultList
     
-    
-    while(list1 && list2){
-        if(list1.val <= list2.val){
-            node.next = list1
-            
-            node = list1
-            
-            list1 = list1.next
-        }else{
-            
-            node.next = list2
-            node = list2
-            
-            list2 =list2.next
-        }
+  
         
+     while(list1 && list2){
+       if(list1.val <= list2.val){
+                node.next = list1
+           
+                node = list1
+               
+               list1 =list1.next
+           
+           } else{
+               node.next = list2
+               
+               node = list2
+               
+               list2 =list2.next
+           }
+        }
+
+
+    if(list1 === null) {
+        node.next = list2
+    }
+    if(list2 === null) {
+         node.next = list1
     }
     
-    if(list1 === null)  node.next = list2
-    if(list2 === null)  node.next = list1
     
-    return mergeList.next
+    return resultList.next
     
-};
+    
+    
+}
