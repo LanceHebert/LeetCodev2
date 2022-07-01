b1e679b2f429bf3890cdfd792631711ae962ac71
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-06-30 20:31:23 -0700

    Time: 103 ms (70.05%), Space: 50.5 MB (41.76%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,40 +3,16 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+   if(nums.length === 1)return nums[0]
     
-    let maxNum = nums[0]
+    let subA=nums[0]
     
-    for(let i = 1; i < nums.length;i++){
-       nums[i] = Math.max(nums[i],nums[i] + nums[i-1])
-        maxNum = Math.max(maxNum,nums[i])
+    for(let i =1;i < nums.length;i++){
+        nums[i] = Math.max(nums[i],nums[i-1] + nums[i])
+        
+        subA = Math.max(subA,nums[i])
     }
     
-    return maxNum
+    return subA
     
-    
-    
-    
-    
-    
-    
-    
-    
-//     Brute Force
-//    let currentMaxNum =0
-//     let maxNum = nums[0]
-    
-//    for(let i = 0; i < nums.length;i++){
-//         currentMaxNum= nums[i]
-//         maxNum=Math.max(currentMaxNum,maxNum)
-//        if(i+1 !== nums.length){
-//            for(let j = i+1; j<nums.length;j++){
-//                currentMaxNum += nums[j]
-              
-//                maxNum=Math.max(currentMaxNum,maxNum)
-//            }
-//        } else{           
-//            return Math.max(currentMaxNum,maxNum)
-//        }
-//    } 
-//     return maxNum    
 };
