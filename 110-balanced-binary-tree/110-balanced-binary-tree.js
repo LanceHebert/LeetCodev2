56991fee24685c6b5fbb3db5a2dd86d73a7e823c
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-07-07 14:45:54 -0700

    Time: 85 ms (84.97%), Space: 46.6 MB (93.34%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.js
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -14,19 +14,19 @@ var isBalanced = function(root) {
     
     if(root === null) return true
     
-    
-    function countHeight(node){
-        if(node === null) return 0
-      
-    let left =  countHeight(node.left)
-    let right = countHeight(node.right )
+    function dfs(root){
         
-    if(left === -1 || right === -1 || Math.abs(left -right) > 1 )return -1
-    
-        return Math.max(left,right) + 1
+        if(root === null) return 0
+        
+        let left = 1 + dfs(root.left)
+        let right = 1 + dfs(root.right)
+        
+        if(Math.abs(left-right) > 1) return Infinity
+        
+       return Math.max(left,right)
+        
     }
     
-    if(countHeight(root) === -1) return false
-    return true
+    return dfs(root)===Infinity ? false:true
     
 };
