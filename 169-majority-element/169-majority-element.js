3e4c37865c9c6e60d2daa248c5d9ac5aa22ef832
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-07-07 19:14:04 -0700

    Time: 72 ms (91.41%), Space: 45.9 MB (12.39%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -4,19 +4,8 @@
  */
 var majorityElement = function(nums) {
     
-    let numsHash = {}
-    let biggest =0
+  nums.sort()
     
-    for(let i = 0;i<nums.length;i++){
-        numsHash[nums[i]] = numsHash[nums[i]] + 1 || 1
-        
-        if(numsHash[nums[i]] > biggest){
-            biggest = numsHash[nums[i]]
-        }       
-    }
-    
-    for (const [key, value] of Object.entries(numsHash)) {
- if(value === biggest) return key
-}
+    return nums[Math.floor(nums.length/2)]
     
 };
