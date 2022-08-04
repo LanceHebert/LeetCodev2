684704866fd7b09aa5b166632298d7ee24893f1a
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-03 22:48:02 -0700

    Time: 70 ms (88.31%), Space: 41.6 MB (97.52%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,22 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if(s.length < 2)return false
-    
-    let bracket = { "(":')','{':'}','[':']' }
-    
+    let sHash = {'(':')','{':'}','[':']'}    
     let stack = []
     
-    for(let i=0;i<s.length;i++){
-        if(s[i] in bracket){
-            stack.push(bracket[s[i]])
-        }else{
-            if(stack.pop() !== s[i]){
-                return false
-            }
-        }
+    for(let i = 0;i<s.length;i++){
+       if(s[i] in sHash){
+           stack.push(sHash[s[i]])
+           
+       } else{
+           if(stack.pop() !== s[i]) return false
+       }
+        
     }
-    if(stack.length === 0)return true
-    else return false
     
+   if(stack.length !== 0) return false
+   else return true
+   
 };
