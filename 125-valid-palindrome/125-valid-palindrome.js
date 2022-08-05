fbc180bd45fff80d242097a601837f78c95d9e94
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-04 21:40:23 -0700

    Time: 130 ms (27.88%), Space: 43.9 MB (92.98%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,28 +2,17 @@
  * @param {string} s
  * @return {boolean}
  */
-
-// take string remove uppercase, and non alphanum with regex
-// create two pointers , one start one end, move closer to each other. must be equal
-
-
 var isPalindrome = function(s) {
-    if(s.length === 1){
-        return true
-    }
     
-    let regS = s.replace(/\W|_/g,"").toLowerCase()
-    let left = 0
-    let right = regS.length - 1
-    
-    while(left < right){
-        if(regS[left] !== regS[right]){
-          return false
-        }
-          left++
-            right--
-    }
-    
+   let sRep = s.replace(/\W|_/g,"").toLowerCase()
+   let start = 0
+   let end = sRep.length - 1
+   
+   while(start < end){
+       
+       if(sRep[start] !== sRep[end]) return false
+       start++
+       end--
+   }
     return true
-    
 };
