af75b0a017378e598ef0044ff297a3c6003e1e5b
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-05 19:19:13 -0700

    Time: 78 ms (93.40%), Space: 44.2 MB (61.72%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -7,28 +7,27 @@
  */
 var floodFill = function(image, sr, sc, color) {
     
+    if(image[sr][sc] === color) return image
     
-    let initColor = image[sr][sc]
-    
-    if(initColor === color) return image
+    const initColor = image[sr][sc]
     
     function fill(image,r,c,color,initColor){
-        if(r < 0 || r > image.length - 1 || c < 0 || c > image[0].length - 1 || image[r][c] !== initColor){
-           return  
-        }
+        
+        
+        
+        if(r > image.length-1 || r < 0 || c > image[0].length -1 || c < 0 || image[r][c] !== initColor) return null 
+       
         
         image[r][c] = color
-        
-        fill(image,r + 1,c,color,initColor)
-        fill(image,r - 1,c,color,initColor)
-        fill(image,r,c + 1,color,initColor)
-        fill(image,r,c - 1,color,initColor)
-
+               
+        fill(image, r + 1, c, color,initColor)
+        fill(image, r - 1, c, color,initColor)
+        fill(image, r, c+1, color,initColor)
+        fill(image, r, c-1, color,initColor)
+                
     }
-        fill(image,sr,sc,color,initColor)
-
-        
-        
-   return image 
     
+    fill(image, sr, sc, color,initColor)
+    
+    return image
 };
