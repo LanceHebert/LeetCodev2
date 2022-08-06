bf4b277765e96cab4b48be98ec3cad5a0df45b5c
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-06 16:12:39 -0700

    Time: 142 ms (17.45%), Space: 47.1 MB (59.64%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.js
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -10,20 +10,27 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+
+//create recursion to drop down a level and add a counter, check left counter vs right counter. 
+// dfs(left return dfs + 1, if null return 0), if left or right difference is greater than 1 return -1
+
+
 var isBalanced = function(root) {
-    if(root === null)return true
+     if(root === null)return true
     
-    function dfs(root){
-        if(root === null)return 0
-        
-        let left = dfs(root.left)
-        let right = dfs(root.right)
-        
-        if(left === -1 || right === -1 || Math.abs(left-right) > 1) return -1
-        
-        return Math.max(left,right) + 1
-    }
+   function dfs(node){       
+       if(node === null) return 0
+       
+       let left =  dfs(node.left)
+       let right =  dfs(node.right)
+       
+       if(left === -1 || right === -1 || Math.abs(left - right) > 1 ) return -1       
+       return Math.max(left,right) + 1
+           
+                            
+   }
     
-    if(dfs(root) !== -1)return true
-    else return false
+   if(dfs(root) === -1) return false
+    else return true
+       
 };
