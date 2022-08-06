c793b6f6da23cefe73e761a77cd04b6619eaf534
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-05 22:54:57 -0700

    Time: 122 ms (56.37%), Space: 52.2 MB (52.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -12,16 +12,22 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+
+// helper function,depth first search recursion
+
 var lowestCommonAncestor = function(root, p, q) {
-        
+    
+    
+  
     if(root.val > p.val && root.val > q.val){
-       return lowestCommonAncestor(root.left,p,q)
-    }
-    else if(root.val < p.val && root.val < q.val){
-       return lowestCommonAncestor(root.right,p,q)
-    }
+        return lowestCommonAncestor(root.left,p,q)
+    }else if(root.val < p.val && root.val < q.val){
+        return lowestCommonAncestor(root.right,p,q)
+        }
     else{
         return root
     }
+    
+    
     
 };
