a05fbf0a07d511e1e94b2f100c1a271c34ef4a8d
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-05 20:30:07 -0700

    Time: 140 ms (28.16%), Space: 49.9 MB (94.97%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,17 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+//we will store largest num, compare num1 vs next + num1 take max
 var maxSubArray = function(nums) {
-   if(nums.length === 1)return nums[0]
     
-    let subA=nums[0]
+    let largest = nums[0]
     
-    for(let i =1;i < nums.length;i++){
-        nums[i] = Math.max(nums[i],nums[i-1] + nums[i])
+    
+    for(let i = 1; i < nums.length;i++){
+        nums[i] = Math.max(nums[i],nums[i] + nums[i-1])
         
-        subA = Math.max(subA,nums[i])
+        largest = Math.max(largest,nums[i])
+        
     }
+    return largest
     
-    return subA
+    
     
 };
