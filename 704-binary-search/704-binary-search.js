46babc9b0955740d385ec32e33a193eed72bc304
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-05 18:37:48 -0700

    Time: 85 ms (68.56%), Space: 45.1 MB (56.70%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -3,22 +3,26 @@
  * @param {number} target
  * @return {number}
  */
+
+
+
+
+
 var search = function(nums, target) {
-    let start=0 
-    let end=nums.length - 1
-    let mid = Math.floor((start + end)/2) 
     
-    while(start < end && target !== nums[mid]){
+    let start = 0
+    let end = nums.length - 1
+    let mid = Math.floor((start + end) / 2)
+    
+    while(start < end && nums[mid] !== target){
         if(target > nums[mid]){
-            start = mid + 1
+            start = mid + 1            
         }else{
-            end = mid - 1
+            end =mid - 1           
         }
-        mid = Math.floor((start + end)/2) 
+         mid = Math.floor((start + end) / 2)
     }
-    if(target === nums[mid]){
-        return mid
-    }
-    else return -1
+    if(target === nums[mid]) return mid
+    else return -1    
     
 };
