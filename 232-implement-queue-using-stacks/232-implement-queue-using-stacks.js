a7ffc9e896c76afa6a0a51c5bf1f9f078051c28d
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-06 18:26:31 -0700

    Time: 82 ms (55.70%), Space: 42.6 MB (9.78%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.js
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.js
@@ -1,37 +1,73 @@
-class MyQueue{
-        
-    constructor(){
-       this.pushStack = []
-       this.popStack = []
-    }
-     
-    push(val){
-      this.pushStack.push(val)  
-    }
+
+var MyQueue = function() {
+    this.stack1 = []
+    this.stack2 = []
     
-    pop(){
-        
-        while(this.pushStack.length){
-            this.popStack.push(this.pushStack.pop())
-        }
-        
-        
-        let answer = this.popStack.pop()
-        
-        
-        while(this.popStack.length){
-            this.pushStack.push(this.popStack.pop())
-        }
-        return answer
-    }
     
-    peek(){
-        return this.pushStack[0]
-    }
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x)
     
-    empty(){
-        return this.pushStack.length === 0 
-       
-    }
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    console.log(this.stack1)
+   while(this.stack1.length !== 0){
+       let popped = this.stack1.pop()
+       this.stack2.push(popped)       
+   }
+    console.log(this.stack1)
+    let pop = this.stack2.pop()
+    console.log(pop)
+    while(this.stack2.length !== 0){
+       let popped = this.stack2.pop()
+       this.stack1.push(popped)
+   }
+    return pop
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+   
+ while(this.stack1.length !== 0){
+       let popped = this.stack1.pop()
+      this.stack2.push(popped)       
+   }
     
-}
+   let pop = this.stack2.pop()
+   this.stack2.push(pop)
+    
+    while(this.stack2.length !== 0){
+       let popped = this.stack2.pop()
+      this.stack1.push(popped)       
+   }
+    return pop
+    
+    
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+   return this.stack1.length === 0 ? true:false
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
