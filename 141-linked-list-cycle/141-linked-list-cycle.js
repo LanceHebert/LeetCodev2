ef416ae9c2d33165a0a988403d34fc9439913e2b
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-06 17:46:59 -0700

    Time: 103 ms (60.48%), Space: 44.7 MB (84.00%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -10,18 +10,21 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
+// iterate through the list, use one slow point and one fast point,
+
+
 var hasCycle = function(head) {
+    if(head===null)return false
     
-    
-    let slow = head
     let fast = head
+    let slow = head
     
     while(fast && fast.next){
         fast = fast.next.next
         slow = slow.next
-        
-     if(fast === slow) return true
+        if(fast === slow) return true
     }
     return false
-   
+    
 };
