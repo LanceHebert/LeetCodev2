5b55ae8045798661e6550312bf51708acdc6121e
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-06 22:21:10 -0700

    Time: 84 ms (54.44%), Space: 42 MB (31.31%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,26 +2,25 @@
  * @param {number} n
  * @return {number}
  */
-// 1,2,3,5
-//4
-// 1 x4
-// 1,2,1
-// 2,1,1
-// 2,2
-// 1,1,2
 
-// next = i-1 + i-2
+//  1 2 3 5 8 13
+// last two values added together = new value, store last two values and add 
+// i -1 + i-2 = temp
 
 var climbStairs = function(n) {
     
+    if(n < 3) return n
     
-  let arr = [1,1]
     
-    for(let i = 2; i <= n;i++){
-       const result = arr[0] + arr[1]
-       arr[0] = arr[1]
-        arr[1]= result
+   let num1 = 1
+   let num2 = 2
+   let sum = 0
+    
+    for(let i=2;i < n;i++){
+        sum=num1 + num2
+        num1=num2
+        num2=sum
     }
+    return sum
     
-    return arr[1]
 };
