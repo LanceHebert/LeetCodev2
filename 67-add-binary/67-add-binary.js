38853a26b9df353cf744988463140a59dd42f728
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-09 18:42:43 -0700

    Time: 103 ms (45.62%), Space: 42 MB (95.05%) - LeetHub

--- 67-add-binary/67-add-binary.js
+++ 67-add-binary/67-add-binary.js
@@ -4,14 +4,12 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    let sum =0
-    
-   if(a.length+b.length < 53){
-       return sum = (parseInt(a,2)+parseInt(b,2)).toString(2)
-   }else{
-         sum = BigInt("0b"+a) +BigInt("0b"+b)
-       return sum.toString(2)
-   }
-    
-    
+   bigA= `0b${a}`
+bigB= `0b${b}`
+
+let sum = BigInt(bigA) + BigInt(bigB)
+return sum.toString(2)
+
+
+   
 };
