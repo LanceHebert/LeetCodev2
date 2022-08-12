ad5fc4eb644eb2c4b2bc96b85b0f6c02e3a29a96
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-11 18:03:17 -0700

    Time: 95 ms (67.86%), Space: 44.5 MB (40.87%) - LeetHub

--- 57-insert-interval/57-insert-interval.js
+++ 57-insert-interval/57-insert-interval.js
@@ -4,35 +4,38 @@
  * @return {number[][]}
  */
 
-
+// Iterate through array and check first index and find where it fits 
+// iterate through second array and check where it fits
 
 
 var insert = function(intervals, newInterval) {
     
-    let index = 0
-    
-    let stack = []
-    
-    
-    while(index < intervals.length && intervals[index][1] < newInterval[0]){
-            stack.push(intervals[index])
-            index++
-        }
-    
-    
-        while(index < intervals.length && intervals[index][0] <= newInterval[1]){
-            newInterval[0] = Math.min(intervals[index][0], newInterval[0])
-            newInterval[1] = Math.max(intervals[index][1],newInterval[1])
-            index++
-        }
-    stack.push(newInterval)
-    
+   let index = 0 
+   let zeroIndex = newInterval[0]
+   let oneIndex = newInterval[1]
+   let r = [zeroIndex,oneIndex]
+   
+   let stack = []
+   
+   while(index < intervals.length && intervals[index][1] < zeroIndex){
+       stack.push(intervals[index])
+       index++       
+   }
+      
+   while(index < intervals.length && intervals[index][0] <= oneIndex){
+       
+       zeroIndex =Math.min(intervals[index][0],zeroIndex)
+       oneIndex = (Math.max(oneIndex,intervals[index][1]))
+       
+       index++      
+   }
+    stack.push([zeroIndex,oneIndex])
+   
     while(index < intervals.length){
         stack.push(intervals[index])
-    index++
+        index++
     }
+   
     return stack
-    }
     
-    
-    
+};
