188d55f7f979d2a23995cc432c89c2d8e77991b0
Lance Hebert <81220452+LanceHebert@users.noreply.github.com>
2022-08-14 19:29:36 -0700

    Time: 268 ms (52.15%), Space: 58.7 MB (41.62%) - LeetHub

--- 542-01-matrix/542-01-matrix.js
+++ 542-01-matrix/542-01-matrix.js
@@ -3,28 +3,30 @@
  * @return {number[][]}
  */
 var updateMatrix = function(mat) {
-    //BFS = stain spreadingn out , we want to check each box, BFS uses queue
+    
     let queue = []
-    //we want to iterate through each spot and if its a 0 we make it a 0 in queue with it 0 at base height (third value) . if its not a 0 we make it equal infinity
-   for(let i = 0; i<mat.length;i++){
-        for(let j =0;j<mat[0].length;j++){
+    
+    for(let i = 0; i < mat.length;i++){
+        for(let j = 0;j < mat[0].length;j++){
             if(mat[i][j] === 0) queue.push([i,j,0])
-            else mat[i][j] = Infinity           
+            else mat[i][j] = Infinity            
         }
-   }
-    //this direction variable is a way to check top left right down with a for each
-    let direction = [[1,0],[0,1],[-1,0],[0,-1]]
-    //this is the BFS section, basically we continue to check things while there is something in the queue. First we take from first in queue with shift, then if that spot is greater than the distance like an infinity we replace it with the height. 
-  while(queue.length){
-      let [row,col,dist] = queue.shift()
-      
-      if(mat[row][col] > dist) mat[row][col] = dist
-      //Here we are checking each spot around the location with the foreach directions, we check to make sure its inbounds and if it is we see if its something we havent visited yet by checking to see if it equals infinity. if it hasnt been visited we push it to the queue.
-     direction.forEach(([r,c])=>{
-         if(row+r < mat.length && row+r >= 0 && col+c < mat[0].length && col+c >= 0){
-             if(mat[row+r][col+c] === Infinity) queue.push([row+r,col+c,dist+1])
-         } 
-     })       
-  }
-   return mat 
+    }
+    
+    let dir = [[1,0],[0,1],[-1,0],[0,-1]]
+    
+    while(queue.length){
+        let [row,col,dist] = queue.shift()
+        
+        if(mat[row][col] > dist ) mat[row][col] = dist
+        
+        dir.forEach(([r,c])=>{
+            if(row+r < mat.length && row+r >= 0 && col+c < mat[0].length && col+c >= 0){
+                if(mat[row+r][col+c] === Infinity) queue.push([row+r,col+c,dist+1])
+            }
+        })
+        
+    }
+    return mat
+    
 };
